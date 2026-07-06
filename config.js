a238eb5fef3b550b3702ce8ba6a224e98d9f5138
swallsten <99091840+swallsten@users.noreply.github.com>
2026-07-06 13:44:36 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
    To drive the site from the Google Sheet, paste the two published-CSV links
    here (see EDITOR-SETUP.md), then commit this file. */
 window.TPI_DATA = {
-  speakersCsvUrl: "",
+  speakersCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTPOwbIypDKTg5fsjcH2WbI-aRjOrc1MV66y3YFtO7CSBuG5XoQ2TllKdWQpDW2KUM-eeCkyTTYE6cI/pub?output=csv",
   agendaCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTdPMVhjmbNqjQrjBSTPkCBOdyinQJLE7IilXh4bsh2eu5JKesp-pK6B45sJnELyKwhC1W54q4OyP6u/pub?output=csv"
 };
